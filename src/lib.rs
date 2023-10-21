b0a508cdf8501a4414234486e62c56a19a246325
Llonvne <86454692+Llonvne@users.noreply.github.com>
2023-10-21 17:55:27 +0800

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
+use crate::object::JsonObject;
+use crate::parser::Parser;
+use crate::token::{parse_to_tokens, JsonTokenStream};
+
 pub mod array;
 pub mod keyvalue;
 pub mod object;
 pub mod parser;
 pub mod token;
 pub mod value;
+
+pub struct JsonObjectWrapper<'a> {
+    pub object: JsonObject<'a>,
+    origin: &'a str,
+}
+
+impl<'a> From<&'a str> for JsonObjectWrapper<'a> {
+    fn from(value: &'a str) -> Self {
+        let tokens = parse_to_tokens(value).expect("");
+        let obj = Parser::new(&tokens).parse();
+        JsonObjectWrapper {
+            object: obj,
+            origin: value,
+        }
+    }
+}
